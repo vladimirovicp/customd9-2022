e4ada4916860fa79631159f760aca885088339d2
vladimirovicp <vladimirovicp70@gmail.com>
2022-04-17 01:07:42 +0400

    dummy country

---
+++ public_html/modules/custom/dummy_country_state_city/src/Plugin/Field/FieldFormatter/DummyContryStateCityFormatter.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Drupal\country_state_city\Plugin\Field\FieldFormatter;
+
+/**
+ * Plugin implementation of the 'contry_state_city_formatter' formatter.
+ *
+ * @FieldFormatter(
+ *   id = "dummy_contry_state_city_formatter",
+ *   label = @Translation("Contry state city formatter"),
+ *   field_types = {
+ *     "dummy_country_state_city_type"
+ *   }
+ * )
+ */
