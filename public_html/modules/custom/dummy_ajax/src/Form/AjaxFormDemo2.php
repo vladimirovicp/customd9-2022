2980135bf137fbc7c147070fbf19c2961fc68f01
vladimirovicp <vladimirovicp70@gmail.com>
2022-05-05 12:53:01 +0400

    add: Страна Область Город

--- public_html/modules/custom/dummy_ajax/src/Form/AjaxFormDemo2.php
+++ public_html/modules/custom/dummy_ajax/src/Form/AjaxFormDemo2.php
@@ -23,7 +23,6 @@ class AjaxFormDemo2 extends FormBase{
 
     $state_options = static::getFirstDropdownOptionOptions();
 
-
     if(empty($form_state->getValue('state_dropdown'))){
       $select_option = key($state_options);
     }
@@ -39,57 +38,74 @@ class AjaxFormDemo2 extends FormBase{
     $country_options = static::getCountryOption();
 
     if(empty($form_state->getValue('country'))){
-      //dpm(key($country_options));
-      $country_default_value = key($country_options);
-
       $country_default_value = '-- Select an option --';
-
     }
     else {
       $country_default_value = $form_state->getValue('country');
     }
 
-    //$country_default_value
+//    $state_options = static::getStateOption();
+//
+//    if(empty($form_state->getValue('state'))){
+//      $state_default_value = '-- Select an option --';
+//    }
+//    else {
+//      $state_default_value = $form_state->getValue('state');
+//    }
 
 
-    $form['country'] = [
+
+    $form['country_state_city_container'] = [
+      '#type' => 'container',
+      '#attributes' => ['id' => 'country-state-city-container'],
+    ];
+
+    $form['country_state_city_container']['country_container']['country'] = [
       '#type' => 'select',
       '#title' => $this->t('Country'),
       '#empty_option' => $country_default_value,
       '#options' => $country_options,
       '#ajax' => [
-        'callback' => '::instrumentCityCallback',
-        'wrapper' => 'city-fieldset-container2',
+        'callback' => '::instrumentCountryCallback',
+        'wrapper' => 'state-container',
+        'event' => 'change',
+      ],
+    ];
+
+    $form['country_state_city_container']['state_container'] = [
+      '#type' => 'container',
+      '#attributes' => ['id' => 'state-container'],
+    ];
+
+    $state_default_value = !empty($form_state->getValue('state')) ? $form_state->getValue('state') : 0;
+
+    $form['country_state_city_container']['state_container']['state'] = [
+      '#type' => 'select',
+      '#title' => $this->t('State'),
+      '#options' => static ::getStateOption($country_default_value),
+      '#default_value' => $state_default_value,
+      '#ajax' => [
+        'callback' => '::instrumentStateCallback',
+        'wrapper' => 'city-container',
         'event' => 'change',
       ],
     ];
 
 
-   // dpm(static ::getCityOption($country_default_value));
-
-
-    $form['city_fieldset_container'] = [
+    $form['country_state_city_container']['city_container'] = [
       '#type' => 'container',
-      '#attributes' => ['id' => 'city-fieldset-container2'],
-    ];
-
-    $form['city_fieldset_container']['select_fieldset'] = [
-      '#type' => 'fieldset',
-      '#title' => $this->t('City'),
+      '#attributes' => ['id' => 'city-container'],
     ];
 
 
-    $form['city_fieldset_container']['select_fieldset']['city'] = [
+    $form['country_state_city_container']['city_container']['city'] = [
       '#type' => 'select',
       '#title' => $this->t('City'),
-      '#options' => static ::getCityOption($country_default_value),
-      '#default_value' => !empty($form_state->getValue('city')) ? $form_state->getValue('city') : 'none',
+      '#options' => static ::getCityOption($state_default_value),
+      '#default_value' => !empty($form_state->getValue('city')) ? $form_state->getValue('city') : '-- Select an option --',
     ];
 
-//    '#type' => 'select',
-//      '#title' => $state_options[$select_option] . '  ' . $this->t('State'),
-//      '#options' => static ::getSecondDropdownOptions($select_option),
-//      '#default_value' => !empty($form_state->getValue('select_drobdown')) ? $form_state->getValue('select_dropdown') : 'none',
+
 
     $form['option_state_fieldset'] = [
       '#type' => 'fieldset',
@@ -132,8 +148,10 @@ class AjaxFormDemo2 extends FormBase{
       '#value' => $this->t('Submit'),
     ];
 
+    //dpm($form['select_fieldset_container']['select_fieldset']['select_dropdown']);
+
     if($select_option == 'none'){
-      $form['select_fieldset_container']['select_fieldset']['select_dropdown']['title'] = $this->t('dfgdfghfg');
+      //$form['select_fieldset_container']['select_fieldset']['select_dropdown']['title'] = $this->t('dfgdfghfg');
       $form['select_fieldset_container']['select_fieldset']['select_dropdown']['#disabled'] = true;
       $form['select_fieldset_container']['select_fieldset']['submit']['#disabled'] = true;
     }
@@ -141,8 +159,13 @@ class AjaxFormDemo2 extends FormBase{
     //dpm($country_default_value);
 
     if($country_default_value == '-- Select an option --'){
-      $form['city_fieldset_container']['select_fieldset']['city']['title'] = $this->t('dfgdfghfg');
-      $form['city_fieldset_container']['select_fieldset']['city']['#disabled'] = true;
+      //$form['city_fieldset_container']['select_fieldset']['city']['title'] = $this->t('dfgdfghfg');
+      //$form['city_fieldset_container']['select_fieldset']['city']['#disabled'] = true;
+
+      //$form['city_fieldset_container']['select_fieldset']['city']['title'] = $this->t('dfgdfghfg');
+      $form['country_state_city_container']['state_container']['state']['#disabled'] = true;
+
+      $form['country_state_city_container']['city_container']['city']['#disabled'] = true;
     }
 
 
@@ -170,19 +193,21 @@ class AjaxFormDemo2 extends FormBase{
   }
 
 
-  public function instrumentCityCallback(array $form, FormStateInterface $form_state) {
+  public function instrumentCountryCallback(array $form, FormStateInterface $form_state) {
     //return $form['city'];
-    return $form['city_fieldset_container'];
+   // return $form['city_fieldset_container'];
+    //return $form['country_state_city_container']['city_container'];
+    return $form['country_state_city_container']['state_container'];
+  }
+
+  public function instrumentStateCallback(array $form, FormStateInterface $form_state) {
+    return $form['country_state_city_container']['city_container'];
   }
 
 
   public static function getCountryOption(){
-
     $module_handler = \Drupal::service('module_handler');
     $module_path = $module_handler->getModule('dummy_ajax')->getPath();
-
-    //$country_options = ['-- Select an option --'];
-
 
     $i = 0;
     $handle = @fopen($module_path . '/files/countries.csv', "r");
@@ -209,36 +234,86 @@ class AjaxFormDemo2 extends FormBase{
     return  $country_options;
   }
 
-  public static function getCityOption($key = ''){
-//    dpm('|||');
-//    dpm($key);
-    switch ($key){
-      case 1:
-        $options = [
-          '1' => 'Первый 1',
-          '2' => 'Первый 2',
-          '3' => 'Первый 3',
-        ];
-        break;
 
-      case 'y':
-        $options = [
-          'Второй 1' => 'Второй 1',
-          'Второй 2' => 'Второй 2',
-        ];
-        break;
+  public static function getStateOption($idCountry){
 
-      default:
-        $options = ['none' => 'none'];
-        break;
+
+    $state_options[0] = '-- Select an option --';
+    if ($idCountry == 0){
+      return $state_options;
     }
-    return $options;
+
+    $module_handler = \Drupal::service('module_handler');
+    $module_path = $module_handler->getModule('dummy_ajax')->getPath();
+
+    $i = 0;
+    $handle = @fopen($module_path . '/files/states/state' . $idCountry . '.csv', "r");
+
+    if ($handle) {
+      while (($row = fgetcsv($handle,  4096, ';')) !== FALSE) {
+        if (empty($fields)) {
+          $fields = $row;
+          continue;
+        }
+        foreach ($row as $k => $value) {
+          $array[$i][$fields[$k]] = $value;
+          $state_options[$array[$i]['id']] = $array[$i]['nameRu'];
+        }
+        $i++;
+      }
+      if (!feof($handle)) {
+        echo "Error: unexpected fgets() fail\n";
+      }
+      fclose($handle);
+
+    }
+
+    return  $state_options;
+
+
+  }
+
+
+  public static function getCityOption($idState){
+
+    $sities_options[0] = '-- Select an option --';
+    if ($idState == 0){
+      return $sities_options;
+    }
+
+    $module_handler = \Drupal::service('module_handler');
+    $module_path = $module_handler->getModule('dummy_ajax')->getPath();
+
+    $i = 0;
+    $handle = @fopen($module_path . '/files/cities/city_' . $idState . '.csv', "r");
+
+    if ($handle) {
+      while (($row = fgetcsv($handle,  4096, ';')) !== FALSE) {
+        if (empty($fields)) {
+          $fields = $row;
+          continue;
+        }
+        foreach ($row as $k => $value) {
+          $array[$i][$fields[$k]] = $value;
+          $sities_options[$array[$i]['id']] = $array[$i]['nameRu'];
+        }
+        $i++;
+      }
+      if (!feof($handle)) {
+        echo "Error: unexpected fgets() fail\n";
+      }
+      fclose($handle);
+
+    }
+
+    return  $sities_options;
+
   }
 
 
   public static function getFirstDropdownOptionOptions(){
       return[
-        'none' => 'none',
+        'none10' => 'none10',
         'x' => 'x',
         'y' => 'y',
       ];
