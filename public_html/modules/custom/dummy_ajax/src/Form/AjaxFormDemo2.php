954a5caa53751fdc3171bda65b3f0ec6c2415008
vladimirovicp <vladimirovicp70@gmail.com>
2022-05-05 13:02:19 +0400

    закоментил лишнее

--- public_html/modules/custom/dummy_ajax/src/Form/AjaxFormDemo2.php
+++ public_html/modules/custom/dummy_ajax/src/Form/AjaxFormDemo2.php
@@ -21,14 +21,14 @@ class AjaxFormDemo2 extends FormBase{
   public function buildForm(array $form, FormStateInterface $form_state){
 
 
-    $state_options = static::getFirstDropdownOptionOptions();
-
-    if(empty($form_state->getValue('state_dropdown'))){
-      $select_option = key($state_options);
-    }
-    else {
-      $select_option = $form_state->getValue('state_dropdown');
-    }
+//    $state_options = static::getFirstDropdownOptionOptions();
+//
+//    if(empty($form_state->getValue('state_dropdown'))){
+//      $select_option = key($state_options);
+//    }
+//    else {
+//      $select_option = $form_state->getValue('state_dropdown');
+//    }
 
 
 
@@ -107,41 +107,41 @@ class AjaxFormDemo2 extends FormBase{
 
 
 
-    $form['option_state_fieldset'] = [
-      '#type' => 'fieldset',
-      '#title' => $this->t('Choose State'),
-    ];
+//    $form['option_state_fieldset'] = [
+//      '#type' => 'fieldset',
+//      '#title' => $this->t('Choose State'),
+//    ];
 
 
-    $form['option_state_fieldset']['state_dropdown'] = [
-      '#type' => 'select',
-      '#title' => $this->t('State'),
-      '#options' => $state_options,
-      '#default_value' => $select_option,
-      '#ajax' => [
-        'callback' => '::instrumentDropdownCallback',
-        'wrapper' => 'state-fieldset-container',
-        'event' => 'change',
-      ],
-    ];
+//    $form['option_state_fieldset']['state_dropdown'] = [
+//      '#type' => 'select',
+//      '#title' => $this->t('State'),
+//      '#options' => $state_options,
+//      '#default_value' => $select_option,
+//      '#ajax' => [
+//        'callback' => '::instrumentDropdownCallback',
+//        'wrapper' => 'state-fieldset-container',
+//        'event' => 'change',
+//      ],
+//    ];
 
 
-    $form['select_fieldset_container'] = [
-      '#type' => 'container',
-      '#attributes' => ['id' => 'state-fieldset-container'],
-    ];
+//    $form['select_fieldset_container'] = [
+//      '#type' => 'container',
+//      '#attributes' => ['id' => 'state-fieldset-container'],
+//    ];
+//
+//    $form['select_fieldset_container']['select_fieldset'] = [
+//      '#type' => 'fieldset',
+//      '#title' => $this->t('Choose an one'),
+//    ];
 
-    $form['select_fieldset_container']['select_fieldset'] = [
-      '#type' => 'fieldset',
-      '#title' => $this->t('Choose an one'),
-    ];
-
-    $form['select_fieldset_container']['select_fieldset']['select_dropdown'] = [
-      '#type' => 'select',
-      '#title' => $state_options[$select_option] . '  ' . $this->t('State'),
-      '#options' => static ::getSecondDropdownOptions($select_option),
-      '#default_value' => !empty($form_state->getValue('select_drobdown')) ? $form_state->getValue('select_dropdown') : 'none',
-    ];
+//    $form['select_fieldset_container']['select_fieldset']['select_dropdown'] = [
+//      '#type' => 'select',
+//      '#title' => $state_options[$select_option] . '  ' . $this->t('State'),
+//      '#options' => static ::getSecondDropdownOptions($select_option),
+//      '#default_value' => !empty($form_state->getValue('select_drobdown')) ? $form_state->getValue('select_dropdown') : 'none',
+//    ];
 
     $form['select_fieldset_container']['select_fieldset']['submit'] = [
       '#type' => 'submit',
@@ -150,11 +150,11 @@ class AjaxFormDemo2 extends FormBase{
 
     //dpm($form['select_fieldset_container']['select_fieldset']['select_dropdown']);
 
-    if($select_option == 'none'){
-      //$form['select_fieldset_container']['select_fieldset']['select_dropdown']['title'] = $this->t('dfgdfghfg');
-      $form['select_fieldset_container']['select_fieldset']['select_dropdown']['#disabled'] = true;
-      $form['select_fieldset_container']['select_fieldset']['submit']['#disabled'] = true;
-    }
+//    if($select_option == 'none'){
+//      //$form['select_fieldset_container']['select_fieldset']['select_dropdown']['title'] = $this->t('dfgdfghfg');
+//      $form['select_fieldset_container']['select_fieldset']['select_dropdown']['#disabled'] = true;
+//      $form['select_fieldset_container']['select_fieldset']['submit']['#disabled'] = true;
+//    }
 
     //dpm($country_default_value);
 
@@ -165,38 +165,27 @@ class AjaxFormDemo2 extends FormBase{
       //$form['city_fieldset_container']['select_fieldset']['city']['title'] = $this->t('dfgdfghfg');
       $form['country_state_city_container']['state_container']['state']['#disabled'] = true;
 
+
+    }
+
+    if($state_default_value == 0){
       $form['country_state_city_container']['city_container']['city']['#disabled'] = true;
     }
+
 
 
 
     return $form;
   }
 
-  /**
-   * Setting the message in our form.
-   */
-//  public function setMessage(array $form, FormStateInterface $form_state) {
-//
-//    $response = new AjaxResponse();
-//    $response->addCommand(
-//      new HtmlCommand(
-//        '.result_message',
-//        '<div class="my_top_message">' . t('The results is @result', ['@result' => ($form_state->getValue('number_1') + $form_state->getValue('number_2'))]) . '</div>'
-//      )
-//    );
-//    return $response;
-//  }
 
-  public function instrumentDropdownCallback(array $form, FormStateInterface $form_state) {
-    return $form['select_fieldset_container'];
-  }
+
+//  public function instrumentDropdownCallback(array $form, FormStateInterface $form_state) {
+//    return $form['select_fieldset_container'];
+//  }
 
 
   public function instrumentCountryCallback(array $form, FormStateInterface $form_state) {
-    //return $form['city'];
-   // return $form['city_fieldset_container'];
-    //return $form['country_state_city_container']['city_container'];
     return $form['country_state_city_container']['state_container'];
   }
 
@@ -228,15 +217,12 @@ class AjaxFormDemo2 extends FormBase{
         echo "Error: unexpected fgets() fail\n";
       }
       fclose($handle);
-
     }
-
     return  $country_options;
   }
 
 
   public static function getStateOption($idCountry){
-
 
     $state_options[0] = '-- Select an option --';
     if ($idCountry == 0){
@@ -267,15 +253,11 @@ class AjaxFormDemo2 extends FormBase{
       fclose($handle);
 
     }
-
     return  $state_options;
-
-
   }
 
 
   public static function getCityOption($idState){
-
     $sities_options[0] = '-- Select an option --';
     if ($idState == 0){
       return $sities_options;
@@ -303,48 +285,46 @@ class AjaxFormDemo2 extends FormBase{
         echo "Error: unexpected fgets() fail\n";
       }
       fclose($handle);
-
     }
-
     return  $sities_options;
 
   }
 
 
-  public static function getFirstDropdownOptionOptions(){
-      return[
-        'none10' => 'none10',
-        'x' => 'x',
-        'y' => 'y',
-      ];
-  }
+//  public static function getFirstDropdownOptionOptions(){
+//      return[
+//        'none10' => 'none10',
+//        'x' => 'x',
+//        'y' => 'y',
+//      ];
+//  }
 
 
-  public static function getSecondDropdownOptions($key = ''){
-    switch ($key){
-      case 'x':
-        $options = [
-          'Первый 1' => 'Первый 1',
-          'Первый 2' => 'Первый 2',
-        ];
-        break;
-
-      case 'y':
-        $options = [
-          'Второй 1' => 'Второй 1',
-          'Второй 2' => 'Второй 2',
-        ];
-        break;
-
-      default:
-        $options = ['none' => 'none'];
-        break;
-
-
-    }
-
-    return $options;
-  }
+//  public static function getSecondDropdownOptions($key = ''){
+//    switch ($key){
+//      case 'x':
+//        $options = [
+//          'Первый 1' => 'Первый 1',
+//          'Первый 2' => 'Первый 2',
+//        ];
+//        break;
+//
+//      case 'y':
+//        $options = [
+//          'Второй 1' => 'Второй 1',
+//          'Второй 2' => 'Второй 2',
+//        ];
+//        break;
+//
+//      default:
+//        $options = ['none' => 'none'];
+//        break;
+//
+//
+//    }
+//
+//    return $options;
+//  }
 
 
 
